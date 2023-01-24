d35581e6054b13e6a0fd4c204014114e6482ff1f
NikiRufolo <nikirufolo@gmail.com>
2023-01-24 16:48:27 +0100

    vue

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,4 +1,18 @@
-@extends('layouts.app')
-@section('content')
-    <h1>Ciao</h1>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Front Office</title>
+</head>
+
+<body>
+    <div id="root">
+
+    </div>
+    <script src="{{ assets('js/app.js') }}"></script>
+</body>
+
+</html>
